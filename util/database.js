1a1fe7892f10728cda004825c2936a1c28e9c19c
Babakar Bara Diop <BABS@MacBook-Pro.local>
2018-11-03 20:48:21 +0100

    added db vars

--- util/database.js
+++ util/database.js
@@ -1,9 +1,9 @@
 const Sequelize= require('sequelize')
 
 // this will automatically set up a connection pool
-const sequelize = new Sequelize('nodejs', 'babakar', 'papillon', {
+const sequelize = new Sequelize('heroku_55e837713f867fc', 'bbfacf12d38bc1', '2bfa5db7', {
   dialect:'mysql',
-  host:'127.0.0.1',
+  host:'us-cdbr-iron-east-01.cleardb.net',
   port:'8889'
 })
 
